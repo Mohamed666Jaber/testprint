33314a1ccbe84247bb304649fb19c61cae9991d1
Mohamed666Jaber <160975800+Mohamed666Jaber@users.noreply.github.com>
2025-11-07 11:47:01 +0100

    Update ft_printf.h

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,14 +6,15 @@
 
 typedef struct s_flags
 {
-	int		minus;
-	int		zero;
-	int		width;
-	int		precision;
-	int		dot;
-	int		hash;
-	int		space;
-	int		plus;
+	int	minus;
+	int	zero;
+	int	dot;
+	int	star;
+	int	width;
+	int	precision;
+	int	hash;
+	int	space;
+	int	plus;
 }	t_flags;
 
 int		ft_print_memory(void *p);
@@ -23,29 +24,22 @@ int		ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_printf(const char *format, ...);
 int		ft_putunbr(unsigned int nbr);
-
-/* Bonus functions */
 int		ft_isdigit(int c);
-int		ft_atoi(const char *str);
 t_flags	ft_init_flags(void);
-t_flags	ft_parse_flags(const char *format, int *i, va_list args);
-int		ft_print_format_bonus(char c, va_list args, t_flags flags);
-
-/* Format handlers with flags */
-int		ft_print_char(char c, t_flags flags);
-int		ft_print_str(char *str, t_flags flags);
-int		ft_print_ptr(void *ptr, t_flags flags);
-int		ft_print_int(int n, t_flags flags);
-int		ft_print_unsigned(unsigned int n, t_flags flags);
-int		ft_print_hex(unsigned int n, char format, t_flags flags);
-int		ft_print_percent(t_flags flags);
-
-/* Helper functions */
-int		ft_putchar_count(char c);
-int		ft_putstr_count(char *str);
-int		ft_putnstr(char *str, int n);
-int		ft_print_width(int width, int zero);
-int		ft_numlen(long long n);
-int		ft_hexlen(unsigned long n);
+int		ft_parse_flags(const char *format, int i, t_flags *flags, va_list args);
+int		ft_handle_char(char c, t_flags flags);
+int		ft_handle_string(char *str, t_flags flags);
+int		ft_handle_pointer(void *p, t_flags flags);
+int		ft_handle_int(int n, t_flags flags);
+int		ft_handle_unsigned(unsigned int n, t_flags flags);
+int		ft_handle_hex(unsigned int n, int uppercase, t_flags flags);
+int		ft_handle_percent(t_flags flags);
+int		ft_putchar_len(char c);
+int		ft_putstr_len(char *str);
+int		ft_putnbr_len(long long n);
+int		ft_putunbr_len(unsigned int n);
+char	*ft_uitoa(unsigned int n);
+int		ft_num_len(long long n, int base);
+int		ft_unum_len(unsigned long long n, int base);
 
 #endif
